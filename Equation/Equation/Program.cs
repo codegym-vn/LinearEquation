cdc8e2326a1e642abb796fb545ffe500114969d8
tungnk_2711 <tungdemo@gmail.com>
2018-04-18 23:23:28 +0700

    update project

--- Equation/Equation/Program.cs
+++ Equation/Equation/Program.cs
@@ -10,30 +10,30 @@ namespace Equation
     {
         static void Main(string[] args)
         {
+            //Khai báo các biến sử dụng trong chương trình
+            float a, b, result;
+
             //Nhập hệ số a
             Console.Write("Enter a:");
-            float a = float.Parse(Console.ReadLine());
+            a = float.Parse(Console.ReadLine());
 
             //Nhập hệ số b
             Console.Write("Enter b:");
-            float b = float.Parse(Console.ReadLine());
+            b = float.Parse(Console.ReadLine());
 
             if (a == 0)
             {
-                Console.WriteLine("The equation has no solution.");
+                if (b == 0)
+                    Console.WriteLine("Equation countless solutions.");
+                else
+                    Console.WriteLine("The equation has no solution.");
             }
             else
             {
-                if (b == 0)
-                {
-                    Console.WriteLine("Equation number of solutions");
-                }
-                else
-                {
-                    float result = -b / a;
-                    Console.WriteLine("The solution of the equation is {0}", result);
-                }
+                result = -b / a;
+                Console.WriteLine("The solution of the equation is {0}.", result);
             }
+
             Console.ReadLine();
         }
     }
